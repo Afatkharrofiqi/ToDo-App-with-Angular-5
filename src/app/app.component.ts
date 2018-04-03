c0ef9fb0b269d6ffe3da527c469ff4dd899e184b
Achmad Fatkharrofiqi <achmadfatkharrofiqi404@gmail.com>
2018-04-03 07:05:02 +0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import {Component }from '@angular/core'; 
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+interface Task{
+	title: string,
+	is_canceled: boolean
+}
+
+@Component( {
+	selector:'app-root', 
+	templateUrl:'./app.component.html', 
+	styleUrls:['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	tasks: Array<Task> = [
+		{
+			title: "Go Home",
+			is_canceled: false
+		},
+		{
+			title: "Take a nap",
+			is_canceled: false
+		},
+		{
+			title: "Start learning with Achmad",
+			is_canceled: false
+		}
+	]
+
+  	clearToDo() {
+		let do_delete = confirm("Are you sure to delete all tasks?");
+		if(do_delete){
+			this.tasks.splice(0); 
+		}
+	}
+
+	addTask(value:string) {
+		this.tasks.push(
+			{
+				title: value,
+				is_canceled: false
+			}			
+		); 
+	}
+
+	cancelTask(idx: number){
+		if(this.tasks[idx].is_canceled){
+			this.tasks[idx].is_canceled = false;
+		}else{
+			this.tasks[idx].is_canceled = true;
+		}		
+	}
+
+	deleteTask(idx: number){
+		let do_delete = confirm("Are you sure to delete the task?");
+		if(do_delete){
+			this.tasks.splice(idx, 1);
+		}
+	}
 }
